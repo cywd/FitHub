96deff6e1832e5847058b20e101e1bdfe867e92d
Cyrill <lichunyang@outlook.com>
2017-12-07 13:42:42 +0800

    fix code style

--- FitHub/Pods/Target Support Files/FitRefresh/FitRefresh-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_FitRefresh : NSObject
-@end
-@implementation PodsDummy_FitRefresh
-@end
--- FitHub/Pods/Target Support Files/Pods-FitHub/Pods-FitHub-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FitHub : NSObject
-@end
-@implementation PodsDummy_Pods_FitHub
-@end
