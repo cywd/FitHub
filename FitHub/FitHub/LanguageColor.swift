a91dd3891f8c6772d7147c835ad0428674d956e2
cywd <sakura_29@163.com>
2017-12-28 18:07:15 +0800

    add some tool class

---
+++ FitHub/FitHub/LanguageColor.swift
@@ -0,0 +1,32 @@
+//
+//  LanguageColor.swift
+//  FitHub
+//
+//  Created by cyrill on 2017/12/28.
+//  Copyright © 2017年 cyrill.win. All rights reserved.
+//
+
+import UIKit
+
+class LanguageColor: NSObject {
+    // <span class="repo-language-color ml-0" style="background-color:#4F5D95;"></span>
+    // c   #555555
+    // c++ #f34b7d
+    // c#  #178600
+    // oc  #438eff
+    // python #3572A5
+    // java #b07219
+    // php #4F5D95
+    // asp #6a40fd
+    // js #f1e05a
+    // go background-color:#375eab;
+    // shell background-color:#89e051;
+    // css background-color:#563d7c;
+    // ruby background-color:#701516;
+    // cmake background-color:#701516;
+    // makefile background-color:#427819;
+    // swift background-color:#ffac45;
+    // scala background-color:#c22d40;
+    // 
+    
+}
